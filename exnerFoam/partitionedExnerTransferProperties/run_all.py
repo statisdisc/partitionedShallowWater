3e4b95b29520445a48092e44dd996ab85b9919d7
Will <will.a.m@hotmail.co.uk>
2018-11-05 22:35:12 +0000

    More transfer test cases.

---
+++ exnerFoam/partitionedExnerTransferProperties/run_all.py
@@ -0,0 +1,9 @@
+import os,sys
+
+directory = sys.path[0]
+
+for subdir in os.listdir(directory):
+    dir = os.path.join(directory, subdir)
+    if os.path.isdir( dir ):
+        dat_file = os.path.join(subdir, "transferDiags.dat")
+        os.system( "cp {} ~/Dropbox/{}.dat".format(dat_file,subdir[3:]) )
