a3e92e6df12329713efe5109e95f5f314a167db6
Will <will.a.m@hotmail.co.uk>
2018-11-10 10:09:59 +0000

    Added test cases for cell centred and cell face transfers.

--- exnerFoam/partitionedExnerTransferProperties/run_all.py
+++ exnerFoam/partitionedExnerTransferProperties/run_all.py
@@ -7,3 +7,6 @@ for subdir in os.listdir(directory):
     if os.path.isdir( dir ):
         dat_file = os.path.join(subdir, "transferDiags.dat")
         os.system( "cp {} ~/Dropbox/{}.dat".format(dat_file,subdir[3:]) )
+        
+        dat_file = os.path.join(subdir, "diags.dat")
+        os.system( "cp {} ~/Dropbox/totEnergy_{}.dat".format(dat_file,subdir[3:]) )
