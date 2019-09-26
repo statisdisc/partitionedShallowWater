99e2cff1c797fc5bb7964cd0b9feec2f37e03ea2
statisdisc <will.a.m@hotmail.co.uk>
2019-09-26 08:08:52 +0100

    Minor test case changes

--- exnerFoam/partitionedExnerTransferProperties/minSigmaTest/cp_to_all.py
+++ exnerFoam/partitionedExnerTransferProperties/minSigmaTest/cp_to_all.py
@@ -6,8 +6,8 @@ for folder in folders:
     #if folder != "dataFolder":
     if folder != "dataFolder" and folder != "00_oneFluid":
         #os.system( "cp 01_Qn_Rn_massExplicit_thetaImplicit/run_identical.sh {}/".format(folder) )
-        os.system( "cp 01_Qn_Rn_massExplicit_thetaImplicit/run_different.sh {}/".format(folder) )
-        #os.system( "cp 01_Qn_Rn_massExplicit_thetaImplicit/system/controlDict {}/system/".format(folder) )
+        #os.system( "cp 01_Qn_Rn_massExplicit_thetaImplicit/run_different.sh {}/".format(folder) )
+        os.system( "cp 01_Qn_Rn_massExplicit_thetaImplicit/system/controlDict {}/system/".format(folder) )
         #os.system( "cp 01_Qn_Rn_massExplicit_thetaImplicit/system/fvSchemes {}/system/".format(folder) )
         #os.system( "cp 01_Qn_Rn_massExplicit_thetaImplicit/system/setFieldsDict {}/system/".format(folder) )
         #os.system( "cp 01_Qn_Rn_massExplicit_thetaImplicit/init_0/sigma.* {}/init_0/".format(folder) )
